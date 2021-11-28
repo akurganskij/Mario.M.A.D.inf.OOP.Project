2f4ea224939ecce459b87d242cc5167397fcdcc9
Мария Дяченко <mashadyachenko2003@gmail.com>
2021-11-29 01:51:25 +0200

    background for buttons & forms

---
+++ Mario.M.A.D.inf.OOP.Project/Form3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mario.M.A.D.inf.OOP.Project
+{
+    public partial class Form3 : Form
+    {
+        Form lastForm;
+        public Form3(Form frm)
+        {
+            InitializeComponent();
+            lastForm = frm;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
