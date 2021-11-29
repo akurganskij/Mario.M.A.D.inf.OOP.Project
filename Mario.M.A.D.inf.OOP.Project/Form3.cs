04c7d6351fa8b5c0cfaf1accfeb97e41014b8278
Daryushonok <bondarenko.odarka@gmail.com>
2021-11-30 00:32:37 +0300

    Added chrstm tree platform money

--- Mario.M.A.D.inf.OOP.Project/Form3.cs
+++ Mario.M.A.D.inf.OOP.Project/Form3.cs
@@ -19,6 +19,21 @@ namespace Mario.M.A.D.inf.OOP.Project
 
         private void Form3_Load(object sender, EventArgs e)
         {
+            pictureBox2.Location = new Point(350, 880);
+            pictureBox3.Location = new Point(600, 720);
+            pictureBox4.Location = new Point(850, 590);
+            pictureBox5.Location = new Point(1100, 480);
+            pictureBox6.Location = new Point(1500, 380);
+            pictureBox7.Location = new Point(1680, 280);
+            pictureBox8.Location = new Point(50, 600);
+            pictureBox9.Location = new Point(450, 570);
+            pictureBox10.Location = new Point(630, 420);
+            pictureBox11.Location = new Point(700, 180);
+            pictureBox12.Location = new Point(550, 290);
+            pictureBox13.Location = new Point(1800, 960);
+            pictureBox14.Location = new Point(80, 530);
+            pictureBox15.Location = new Point(720, 120);
+            pictureBox16.Location = new Point(1700, 70);
 
         }
 
